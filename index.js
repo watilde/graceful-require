c4f6b70e6ca7202cae86f258ec4cc27e842ccbdf
Daijiro Wachi <daijiro.wachi@gmail.com>
2015-05-10 16:50:32 +0900

    Supports using a name node_modules

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
-var path = require('path');
 module.exports = function (pathname) {
   var req = require;
   return function (module_name) {
     try {
       return req(module_name);
     } catch (err) {
+      var path = require('path');
+      var splitRe = process.platform === 'win32' ? /[\/\\]/ : /\//;
       var resolve = null;
       req.main.paths.some(function (item) {
         if (resolve) return true;
-        item = item.replace('node_modules', pathname);
+        var parts = item.split(splitRe);
+        parts[parts.length - 1] = pathname;
+        item = parts.join(path.sep);
         try {
           var main = path.join(item, module_name, 'package.json');
           main = req(main).main;
